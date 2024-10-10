a50e6b26fa578399f40045def0b52a4528015f91
kareemshono <kareem.shono.7@gmail.com>
2024-10-10 16:06:38 +0300

    v9

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+      <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+<meta name="apple-mobile-web-app-title" content="Octa" />
+<link rel="manifest" href="/site.webmanifest" />
+        </Head>
       <body>
         <Main />
         <NextScript />
